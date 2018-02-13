d7b32a75b56882818e2d42f8d82e32d356fc3989
Chely Ho <adept_inept@yahoo.com>
2018-02-13 17:09:41 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,18 @@
 module Memorable
-  def count
-    all.count
+  module ClassMethods
+    def count
+      all.count
+    end
+
+    def reset_all
+      all.clear
+    end
   end
 
-  def reset_all
-    all.clear
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 
 end
