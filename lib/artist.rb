afd05cb4774a4d083d6c224064c5ee75a4f74d61
Chely Ho <adept_inept@yahoo.com>
2018-02-13 17:01:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,5 +26,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-  
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require 'pry'
 
 class Song
   extend Memorable, Findable
-
+  include Paramble
+  
   attr_accessor :name
   attr_reader :artist
 
