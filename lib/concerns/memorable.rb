41770730fef80a856ca35787f0423bd4fc1b64d6
Chely Ho <adept_inept@yahoo.com>
2018-02-13 16:51:47 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,3 +1,26 @@
 module Memorable
+  #  def self.reset_all
+  #    @@songs.clear
+  #  end
+
+  #  def self.count
+  #    self.all.count
+  #  end
+
+  #  def self.reset_all
+  #    self.all.clear
+  #  end
+
+  #  def self.count
+  #    @@artists.count
+  #  end
+
+  def count
+    all.count
+  end
+
+  def reset_all
+    all.clear
+  end
 
 end
